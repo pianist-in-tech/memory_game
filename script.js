082a274aa2be96db6eb933b4ec97804246e4671a
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2023-01-20 11:23:15 -0700

    added some code on when users should only be able to change only two cards at a time

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const COLORS = [
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
+// it is based on an algorithm called Fisher Yates if you want to research more
 function shuffle(array) {
   let counter = array.length;
 
@@ -48,22 +48,53 @@ function createDivsForColors(colorArray) {
 
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
+  
     newDiv.addEventListener("click", handleCardClick);
-
+    
     // append the div to the element with an id of game
-    gameContainer.append(newDiv);
-  }
-}
+    gameContainer.append(newDiv)
+
+} }
+// newDiv.addEventListener("click", handleCardClick);
 
 function handleCardClick(event) {
   let color = event.target.classList.value; 
   event.target.style.backgroundColor = color;
-  
+
+  let clickCounter = 0;
+  let card1 = null;
+  let card2 = null; 
+  if (card1 = null) {card1 = event.target};
+  if (card2 = null) {card2 = event.target};
+  if (card1 = !null && card2 != null && card2 != card1) {
+  clickCounter +2;
+  };
+  if (clickCounter > 2){
+    return};  
+  console.log (clickCounter); 
 }
 
-// when the DOM loads
-createDivsForColors(shuffledColors);
+    // let clickCounter = [];
+    //
+    //   newDiv.addEventListener("click", function(event) {
+    // 
+//   clickCounter++;
+    //   console.log (clickCounter)
+    // });
+    
+    // if (clickCounter.length < 3){
+    //   newDiv.addEventListener("click", handleCardClick);
+    // }
 
-/* */
+// let clickEvent = newDiv.addEventListener("click", handleCardClick);
+// let numOfEvents = [];
+// if (clickEvent){
+//   numOfEvents.push(1);
+// }
+// if (numOfEvents.length < 3) {
+//   handleCardClick;
+// } else { };
+
+
+// when the DOM loads
+createDivsForColors(shuffledColors); 
